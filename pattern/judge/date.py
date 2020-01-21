79f75f45cb222aadd00a775c9911f90ba273e02c
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-01-21 18:06:50 +0100

    feat(date): add method to judge a German date

---
+++ pattern/judge/date.py
@@ -0,0 +1,50 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import re
+
+
+def validate(item: str) -> float:
+    result = [strategy(item) for strategy in [
+        day_month_year,
+        day_name_year,
+    ]]
+    return max(result)
+
+
+MONTH = (r'(?P<month>Januar|Februar|März|April|Mai|Juni|Juli|'
+         r'August|September|Oktober|November|Dezember)')
+
+
+def day_month_year(item: str):
+    pattern = r'(?P<day>\d{1,2})\.(?P<month>\d{1,2})\.(?P<year>\d{4})'
+    matched = re.match(pattern, item)
+    if matched:
+        day = int(matched['day'])
+        month = int(matched['month'])
+        year = int(matched['year'])
+        if not 1 <= day <= 31:
+            return 0.0
+        if not 1 <= month <= 12:
+            return 0.0
+        if not 0 <= year <= 2050:
+            return 0.0
+        return 1.0
+    return 0.0
+
+
+def day_name_year(item: str):
+    pattern = (r'(?P<day>\d{1,2})\.[ ]{0,1}'
+               f'{MONTH}'
+               r'[ ]{0,1}'
+               r'(?P<year>\d{4})')
+    matched = re.match(pattern, item)
+    if matched:
+        return 1.0
+    return 0.0
---
+++ tests/test_judge_date.py
@@ -0,0 +1,28 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+import pytest
+
+import pattern.checker
+
+
+@pytest.mark.parametrize('item', [
+    '20.10.2020',
+    '20. Oktober 2020',
+])
+def test_is_date_valid(item):
+    valid = pattern.checker.is_date(item)
+    assert valid >= 1.0, item
+
+
+@pytest.mark.parametrize('item', [
+    '40.10.2020',
+])
+def test_is_date_invalid(item):
+    valid = pattern.checker.is_date(item)
+    assert valid == 0.0, item
